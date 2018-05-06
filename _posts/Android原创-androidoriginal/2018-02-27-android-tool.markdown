689a7a44fe80c6bf8a79baad2d19d12a8fff44a1
mengzhang <meng.zhang@ihandysoft.com>
2018-05-06 15:51:37 +0800

    change website theme

---
+++ _posts/Android原创-androidoriginal/2018-02-27-android-tool.markdown
@@ -0,0 +1,36 @@
+---
+layout: post
+title: 开发工具
+date:  2018-02-27 16:40:00 +0900  
+description: 开发工具
+img: post-4.jpg # Add image post (optional)
+tags: [Android,tools]
+author: # Add name author (optional)
+androidoriginal: true
+---
+
+**主要记录一些工具：**
+
+
+
+- **SQLiteStudio**	可查看APP里面的数据库，不用root
+
+- **LICEcap**	录制动态图片
+
+- **WinSCP**	windows下连接linux系统文件管理软件
+
+- **IObit Unlocker**	解锁占用文件的进程
+
+- **Navicat for MySQL**	远程连接数据库
+
+- **SourceTree、github desktop**	git客户端
+
+- **Astrill**	不错的国外的VPN客户端
+
+- **jmeter、tsung、appium、Uiautomator**	压力测试工具，自动化测试工具
+
+- **uiautomatorviewer**	用来扫描和分析Android应用程序的UI控件的工具。
+
+- **Atom，NotePad++、UltraEdit** 文件编辑器，**强烈推荐Atom**，它是Github的一款文件编辑器，可关联git仓库，可预览Markdown文件，可以导入文件夹结构，很好用，界面看着也舒服
+
+- **禅道，JIRA** bug管理平台
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,34 @@
-/*
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$(document).ready(function() {
 
-(function($) {
+  // Menu Settings
+  $('.menu-icon, .menu-icon-close').click(function(e) {
+    e.preventDefault();
+    $('.flex-container').toggleClass('active');
+  });
 
-	skel.breakpoints({
-		wide: '(min-width: 961px) and (max-width: 1880px)',
-		normal: '(min-width: 961px) and (max-width: 1620px)',
-		narrow: '(min-width: 961px) and (max-width: 1320px)',
-		narrower: '(max-width: 960px)',
-		mobile: '(max-width: 736px)'
-	});
+  // Search Settings
+  $('.search-icon').on('click', function(e){
+    e.preventDefault();
+    $('.search-box').toggleClass('search-active');
 
-	$(function() {
+    if ($('.search-box').hasClass('search-active')) {
+      $('.search-icon-close').on('click', function(e){
+  		e.preventDefault();
+  		$('.search-box').removeClass('search-active');
+  	});
+  }
+  });
 
-		var	$window = $(window),
-			$body = $('body');
+});
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
-
-		// Scrolly links.
-			$('.scrolly').scrolly();
-
-		// Nav.
-			var $nav_a = $('#nav a.scrolly');
-
-			// Scrolly-fy links.
-				$nav_a
-					.scrolly()
-					.on('click', function(e) {
-
-						var t = $(this),
-							href = t.attr('href');
-
-						if (href[0] != '#')
-							return;
-
-						e.preventDefault();
-
-						// Clear active and lock scrollzer until scrolling has stopped
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
-
-						// Set this link to active
-							t.addClass('active');
-
-					});
-
-			// Initialize scrollzer.
-				var ids = [];
-
-				$nav_a.each(function() {
-
-					var href = $(this).attr('href');
-
-					if (href[0] != '#')
-						return;
-
-					ids.push(href.substring(1));
-
-				});
-
-				$.scrollzer(ids, { pad: 200, lastHack: true });
-
-		// Header (narrower + mobile).
-
-			// Toggle.
-				$(
-					'<div id="headerToggle">' +
-						'<a href="#header" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
-
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
-
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#headerToggle, #header, #main')
-						.css('transition', 'none');
-
-	});
-
-})(jQuery);
+$('.top').click(function() {
+  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > $(window).height()) {
+    $('.top').addClass("up");
+  } else {
+    $('.top').removeClass("up");
+  }
+});
